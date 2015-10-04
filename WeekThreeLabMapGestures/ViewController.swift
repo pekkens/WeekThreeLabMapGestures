3e313bb1b669369c884f2d2f99b9bdbe631f5171
Prisca Ekkens <pekkens@mac.com>
2015-10-04 02:42:11 -0700

    Update

--- WeekThreeLabMapGestures/ViewController.swift
+++ WeekThreeLabMapGestures/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pinImageView: UIImageView!
+    @IBOutlet var pinPanGesture: UIPanGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPInPan(sender: AnyObject) {
+        let location = sender.locationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            NSLog("pan Begins \(location)")
+            UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
+                self.pinImageView.transform = CGAffineTransformMakeScale(1.5, 1.5)
+                }, completion: nil)
+            
+        } else if sender.state == UIGestureRecognizerState.Changed{
+            NSLog("panning \(location)")
+            self.pinImageView.center = location
+            
+        } else if sender.state == UIGestureRecognizerState.Ended{
+            NSLog("pan Ends \(location)")
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                self.pinImageView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+            })
+            
+        }
     }
-
-
 }
 
