8ce401912d4acf6702447621a0dd02fcb8ef54ad
Prisca Ekkens <pekkens@mac.com>
2015-10-04 01:23:08 -0700

    Initial Commit

---
+++ WeekThreeLabMapGestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeekThreeLabMapGestures
+//
+//  Created by Prisca Ekkens on 10/4/15.
+//  Copyright © 2015 Prisca Ekkens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
